691ec38610048aa1e5c7c4065120b04d7dc9df80
jbrenorv <joao.breno.ro17@gmail.com>
2022-12-08 09:37:09 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:wifi_manager/wifi_manager.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final wifiManager = WifiManager();
+  String? platformVersion;
+  String? networkSSID;
+
+  @override
+  void initState() {
+    super.initState();
+    _getPlatformVersion();
+    _getConnectionInfo();
+  }
+
+  void _getPlatformVersion() {
+    try {
+      wifiManager
+          .getPlatformVersion()
+          .then((version) => setState(() => platformVersion = version));
+    } on PlatformException catch (e) {
+      setState(() => platformVersion = "Error: ${e.message}");
+    }
+  }
+
+  void _getConnectionInfo() async {
+    String result;
+    try {
+      result = (await wifiManager.getConnectionInfo()) ?? "Deu nulo..";
+      // .then((ssid) => setState(() => networkSSID = ssid));
+    } on PlatformException catch (e) {
+      result = "Error: ${e.message}";
+    }
+    setState(() => networkSSID = result);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Platform verion: $platformVersion'),
+              Text(networkSSID ?? 'Loading SSID...'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
